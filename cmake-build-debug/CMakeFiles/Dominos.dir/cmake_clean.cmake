c330670b8dc099dd813683280729dca07cac227c
EvanStJohn <46731712+EvanStJohn@users.noreply.github.com>
2019-10-22 10:43:27 -0700

    initial project

---
+++ cmake-build-debug/CMakeFiles/Dominos.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dominos.dir/main.cpp.obj"
+  "Dominos.exe"
+  "Dominos.exe.manifest"
+  "Dominos.pdb"
+  "libDominos.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dominos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
